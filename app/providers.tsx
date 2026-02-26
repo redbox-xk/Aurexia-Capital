cb5cd0c5a1263577176279219c26051a1b918ad2
RedBox-XK <85796047+redbox-xk@users.noreply.github.com>
2026-02-26 14:42:21 +0100

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,9 @@
+// app/providers.tsx
 "use client"
 
 import { ReactNode } from "react"
 import { I18nProvider } from "@/lib/i18n"
 
-export default function Providers({
-  children,
-}: {
-  children: ReactNode
-}) {
+export default function Providers({ children }: { children: ReactNode }) {
   return <I18nProvider>{children}</I18nProvider>
 }
