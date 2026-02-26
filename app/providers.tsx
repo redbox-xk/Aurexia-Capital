5b411a9835ba2da548bff5d457b3c19ba6de0e5d
RedBox-XK <85796047+redbox-xk@users.noreply.github.com>
2026-02-26 14:32:55 +0100

    Create providers.tsx

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { ReactNode } from "react"
+import { I18nProvider } from "@/lib/i18n"
+
+export default function Providers({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return <I18nProvider>{children}</I18nProvider>
+}
